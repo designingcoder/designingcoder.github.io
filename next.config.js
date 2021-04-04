9cc248999df1d5729d6ee4a1eb78be2c783761fe
Santosh Shah <s10santosh@yahoo.com>
2021-04-04 20:48:10 +0530

    next config modified

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
-const path = require("path")
 
 module.exports = {
-  basePath: "/",
-  assetPrefix: "/",
-  target: "serverless",
-  webpack: (config) => {
-    config.resolve.alias["~"] = path.resolve(__dirname, "src")
-    return config
-  }
+    basePath: '/',
+    assetPrefix: '/'
 }
