011f6345a773d8162c356e88e667104aa0cb55e9
designingcoder <80530380+designingcoder@users.noreply.github.com>
2021-04-15 17:18:37 +0000

    Deploying to gh-pages from @ designingcoder/designingcoder.github.io@7a55da25e95b89d94a2e7dc6a38a6b64facbc957 🚀

---
+++ _next/static/h3Goth0oCll5PgaWWUDLy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-490828825257dd1ae18c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-42256d1ed25b5b21eb34.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
